27ddf598613840646da827e50afd8fd9ee971fed
hanshikadharma-afk <hanshika.dharma@gmail.com>
2026-07-27 16:35:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const progress = document.getElementById("progress");
+const themeToggle = document.getElementById("themeToggle");
+
+window.addEventListener("scroll", () => {
+  const s = document.documentElement;
+  const p = (s.scrollTop / (s.scrollHeight - s.clientHeight)) * 100;
+  progress.style.width = p + "%";
+});
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
